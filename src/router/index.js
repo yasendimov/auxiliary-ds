4128840745d2dd7b43560f1b13c2bfce7c229ef7
Yasen Dimov <yasen@auterion.com>
2026-03-13 09:16:46 +0200

    feat: add AuxInput with 3 variants, 3 sizes, icons, prefix/suffix, label/hint/error

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
       { path: 'foundation/typography', name: 'foundation-typography', component: () => import('../docs/pages/HomePage.vue') },
       { path: 'foundation/spacing', name: 'foundation-spacing', component: () => import('../docs/pages/HomePage.vue') },
       { path: 'components/button', name: 'components-button', component: () => import('../docs/pages/ButtonPage.vue') },
-      { path: 'components/input', name: 'components-input', component: () => import('../docs/pages/HomePage.vue') },
+      { path: 'components/input', name: 'components-input', component: () => import('../docs/pages/InputPage.vue') },
       { path: 'components/badge', name: 'components-badge', component: () => import('../docs/pages/BadgePage.vue') },
     ],
   },
